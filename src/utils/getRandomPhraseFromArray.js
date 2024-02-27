dace03357e75e3447219b55a1fd94ffdd161fb46
unknown <solarigarciafederico74@gmail.com>
2024-02-27 10:04:49 -0300

    entregable 1 ready

---
+++ src/utils/getRandomPhraseFromArray.js
@@ -0,0 +1,8 @@
+const getRandomPhraseFromArray = arr => {
+
+    const indexRandom = Math.floor(Math.random() * arr.length)
+    return arr[indexRandom]
+
+}
+
+export default getRandomPhraseFromArray
